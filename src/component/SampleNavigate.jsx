e17ca9c003ddf7c931e597d76f75dfc859cf531c
onegun0 <onegun0@naver.com>
2022-07-22 09:08:22 +0900

    22/07/22_miniblog

---
+++ src/component/SampleNavigate.jsx
@@ -0,0 +1,11 @@
+// import { useNavigate } from "react-router-dom";
+
+// function SampleNavigate(props){
+//     const navigate=useNavigate();
+//     const moveToMain=()=>{
+//         navigate("/");
+//     }
+//     return (
+//         ...
+//     )
+// }
